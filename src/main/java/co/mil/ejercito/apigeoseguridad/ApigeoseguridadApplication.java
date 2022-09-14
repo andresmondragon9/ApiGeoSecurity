669c6e51a4c32e51ab1ee1bf5d8bc608c5ae14d5
andresmondragon <andresmondragon91@gmail.com>
2022-09-14 09:00:17 -0500

    version inicial

---
+++ src/main/java/co/mil/ejercito/apigeoseguridad/ApigeoseguridadApplication.java
@@ -0,0 +1,13 @@
+package co.mil.ejercito.apigeoseguridad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApigeoseguridadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApigeoseguridadApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/mil/ejercito/apigeoseguridad/ApigeoseguridadApplicationTests.java
@@ -0,0 +1,13 @@
+package co.mil.ejercito.apigeoseguridad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApigeoseguridadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
